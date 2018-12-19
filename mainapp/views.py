5286c0198bb02b4e45245209cf64e2a647299510
Romeozlove <bistasulove@gmail.com>
2018-12-19 18:31:29 +0530

    Added Models and modified news.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Article
 
 
 def index(request):
@@ -17,8 +18,17 @@ def about(request):
     return render(request, 'mainapp/about.html', context)
 
 def news(request):
+    db_populate()
+    articles = Article.objects.all()
     context = {
+        'articles': articles,
         'current_date': datetime.now(),
         'title': 'News'
     }
     return render(request, 'mainapp/news.html', context)
+
+def db_populate():
+    if Article.objects.count() == 0:
+        Article(title='first item', content='this is the first db content').save()
+        Article(title='second item', content='this is the second db content').save()
+        Article(title='third item', content='this is the third db content').save()
