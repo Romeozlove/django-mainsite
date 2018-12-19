66d2f7b10fe763242a8319f034598746697c8aa3
Romeozlove <bistasulove@gmail.com>
2018-12-19 14:53:14 +0530

    Completed Navbar with routing

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,3 +8,17 @@ def index(request):
         'title': 'Home'
     }
     return render(request, 'mainapp/index.html', context)
+
+def about(request):
+    context = {
+        'current_date': datetime.now(),
+        'title': 'About'
+    }
+    return render(request, 'mainapp/about.html', context)
+
+def news(request):
+    context = {
+        'current_date': datetime.now(),
+        'title': 'News'
+    }
+    return render(request, 'mainapp/news.html', context)
